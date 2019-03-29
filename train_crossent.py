a35c18dbd9d8b2b123780e181264987d7fd441ac
vi <vanvibig@gmail.com>
2019-03-29 11:58:39 +0700

    add in out

--- train_crossent.py
+++ train_crossent.py
@@ -22,6 +22,22 @@ log = logging.getLogger("train")
 
 TEACHER_PROB = 0.5
 
+def create_in_out(phrase_pairs):
+    
+    fx = open("all_lines.txt", "w", encoding='utf-8', errors='ignore')
+    for pair in phrase_pairs:
+        q = ' '.join(t for t in pair[0])
+        a = ' '.join(t for t in pair[1])
+        fx.write(q+'\n')
+        fx.write(a+'\n')
+        fx.write('\n')
+    fx.close()
+
+def createwordlist(emb_dict):
+    fx = open("word_list.txt", "w", encoding='utf-8', errors='ignore')
+    for key, value in emb_dict.items():
+        fx.write(key+'\n')
+    fx.close()
 
 def run_test(test_data, net, end_token, device="cpu"):
     bleu_sum = 0.0
@@ -52,6 +68,10 @@ if __name__ == "__main__":
     os.makedirs(saves_path, exist_ok=True)
 
     phrase_pairs, emb_dict = data.load_data(genre_filter=args.data)
+
+    create_in_out(phrase_pairs)
+    createwordlist(emb_dict)
+
     log.info("Obtained %d phrase pairs with %d uniq words",
              len(phrase_pairs), len(emb_dict))
     data.save_emb_dict(saves_path, emb_dict)
